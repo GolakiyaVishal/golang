40735d071bc90eafccf34ce0bf56409bd84ee96d
goVishal <golakiyavishal812@gmail.com>
2023-08-20 19:23:48 +0530

    add score program, add parentheses program

--- 04algorithms/01_slices.go
+++ 04algorithms/01_slices.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -16,4 +18,76 @@ func main() {
 	c1 = c1[:]
 	fmt.Println(c1)
 	fmt.Println(c1[0])
+
+	fmt.Printf("special count: %d", specialCount([]int{0, 4, 1, 0, 4}, 2))
+	fmt.Printf("special count: %d", specialCount([]int{0, 1, 1, 0, 1}, 2))
+	fmt.Println()
+	fmt.Println("Score:")
+	fmt.Println(getScore([]string{"5", "2", "C", "D", "+"}))
+	fmt.Println()
+	fmt.Println("check for valid parentheses")
+	fmt.Println(isValidParentheses(""))
+}
+
+func specialCount(list []int, x int) int {
+	j := 0
+
+	for i := 0; i < len(list); i++ {
+		if list[i] >= x {
+			j++
+		}
+	}
+	return j
+}
+
+func getScore(list []string) int {
+	sum := 0
+	l1 := make([]int, 0)
+
+	for i := 0; i < len(list); i++ {
+		switch list[i] {
+		case "+":
+			v := l1[len(l1)-1] + l1[len(l1)-2]
+			l1 = append(l1, v)
+			break
+
+		case "C":
+			l1 = l1[:len(l1)-1]
+			break
+
+		case "D":
+			v := l1[len(l1)-1]
+			l1 = append(l1, v*2)
+			break
+
+		default:
+			v1, _ := strconv.Atoi(list[i])
+			l1 = append(l1, v1)
+		}
+	}
+
+	for _, i := range l1 {
+		sum += i
+	}
+
+	return sum
+}
+
+func isValidParentheses(str string) bool {
+	l1 := make([]string, 0)
+	list := []rune(str)
+
+	for _, j := range list {
+		i := string(j)
+		if strings.Contains("({[", i) {
+			l1 = append(l1, i)
+		} else {
+			if i == ")" && l1[len(l1)-1] != "(" || i == "}" && l1[len(l1)-1] != "{" || i == "]" && l1[len(l1)-1] != "[" {
+				return false
+			} else {
+				l1 = l1[:len(l1)-1]
+			}
+		}
+	}
+	return len(l1) == 0
 }
